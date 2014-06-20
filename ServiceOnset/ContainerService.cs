ec005a8b50db7adc4f2cb871ea899661cfb0027f
Hedda Zhang <heddaz@163.com>
2014-06-20 17:29:45 +0800

    Init the blank service project

---
+++ ServiceOnset/ContainerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ServiceOnset
+{
+    public partial class ContainerService : ServiceBase
+    {
+        public ContainerService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
